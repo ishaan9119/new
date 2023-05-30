2bc508bdddc5d222c813e9c9711227608f5ae98c
ishaan9119 <112673566+ishaan9119@users.noreply.github.com>
2023-05-30 13:03:15 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,8 +6,7 @@ terraform {
    }
  }
 }
+# Configure the AWS Provider
 provider "aws" {
-  region                    = "ap-southeast-1"
-  shared_credentials_files  = ["%USERPROFILE%/.aws/credentials"]
-  profile                   = "default"
+  region = "ap-southeast-1"
 }
