8f486b3316e0d5db6ca56f6fcd1c4bbe5eaebaaf
ishaan9119 <112673566+ishaan9119@users.noreply.github.com>
2023-05-30 13:34:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "my_vm" {
    Name = "My EC2 instance",
  }
 }
+resource "aws_instance" "windows" {
+  ami                         = data.aws_ami.Windows_2019.image_id
+  instance_type               = var.windows_instance_types
+  key_name                    = aws_key_pair.my_key_pair.key_name
