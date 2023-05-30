8366a50973e9268f28c4abed0a2e1795ed9ba14b
ishaan9119 <112673566+ishaan9119@users.noreply.github.com>
2023-05-30 15:40:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resource "aws_instance" "my_vm" {
- ami                       = "ami-00706790d2545217d" //Ubuntu AMI
- instance_type             = "t2.micro"
+provider "aws" {
+  access_key = "AKIA4DQMSMAZWV4U2IPL"
+  secret_key = "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"
+  region     = "ap-southeast-1"
+}
 
- tags = {
-   Name = "My EC2 instance",
- }
+resource "aws_instance" "jenkins" {
+  ami           = "ami-00706790d2545217d"
+  instance_type = "t2.micro"
+  key_name = "peering"
+  tags = {
+    Name = "jenkins"
+  }
 }
