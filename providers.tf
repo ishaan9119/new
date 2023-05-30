8401c273e0e63a5db9fe0eef5d1ca02ee6eb4fa1
ishaan9119 <112673566+ishaan9119@users.noreply.github.com>
2023-05-30 12:48:03 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,3 +6,8 @@ terraform {
    }
  }
 }
+provider "aws" {
+  region                    = "ap-southeast-1"
+  shared_credentials_files  = ["%USERPROFILE%/.aws/credentials"]
+  profile                   = "customprofile"
+}
