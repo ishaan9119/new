711ebd0ad70c62e5d7586bd3fafef3b50b8a0120
ishaan9119 <112673566+ishaan9119@users.noreply.github.com>
2023-05-30 14:41:50 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,9 +6,7 @@ terraform {
    }
  }
 }
-# Configure the AWS Provider
 provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = "AKIA4DQMSMAZ4Q6A33GV"
-  secret_key = "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"
+  region                  = "ap-southeast-1"
+  profile                 = "ishaan"
 }
