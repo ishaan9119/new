e0a9c3acbebae460c691fa5e077f581c0e00b190
ishaan9119 <112673566+ishaan9119@users.noreply.github.com>
2023-05-30 13:41:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 resource "aws_instance" "my_vm" {
- ami                       = "ami-065deacbcaac64cf2" //Ubuntu AMI
+ ami                       = "ami-00706790d2545217d" //Ubuntu AMI
  instance_type             = "t2.micro"
 
  tags = {
    Name = "My EC2 instance",
  }
 }
-resource "aws_instance" "my_vm"{
-  ami                         = data.aws_ami.Windows_2019.image_id
-  instance_type               = var.windows_instance_types
-  key_name                    = aws_key_pair.my_key_pair.key_name
